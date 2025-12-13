fbb16b0451442b024df530d56b61774bfe4c535b
Wiktor122333 <139118299+Wiktor122333@users.noreply.github.com>
2025-12-13 10:26:07 +0100

    Add files via upload

---
+++ Day_2/Buraczkowy_2/main_2.py
@@ -0,0 +1,120 @@
+
+# ██████╗░██╗░░░██╗██████╗░░█████╗░░█████╗░███████╗██╗░░██╗░█████╗░░██╗░░░░░░░██╗██╗░░░██╗
+# ██╔══██╗██║░░░██║██╔══██╗██╔══██╗██╔══██╗╚════██║██║░██╔╝██╔══██╗░██║░░██╗░░██║╚██╗░██╔╝
+# ██████╦╝██║░░░██║██████╔╝███████║██║░░╚═╝░░███╔═╝█████═╝░██║░░██║░╚██╗████╗██╔╝░╚████╔╝░
+# ██╔══██╗██║░░░██║██╔══██╗██╔══██║██║░░██╗██╔══╝░░██╔═██╗░██║░░██║░░████╔═████║░░░╚██╔╝░░
+# ██████╦╝╚██████╔╝██║░░██║██║░░██║╚█████╔╝███████╗██║░╚██╗╚█████╔╝░░╚██╔╝░╚██╔╝░░░░██║░░░
+# ╚═════╝░░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░░░░╚═╝░░░
+# 
+# 
+#
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⣰⣏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠆⠀⠀⠀⠻⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⢸⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠘⢧⢰⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⣼⠇⠀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⠀⠀⢠⠀⣿⠀⠀⠹⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣷⡄⢸⣄⠃⠀⡄⠀⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⠀⠀⣿⣿⢨⣿⡆⢠⡀⢴⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠘⠷⠀⣸⣿⠏⣼⣿⠁⢸⣷⡈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⡄⢀⡙⠟⡡⣸⣿⣿⣆⠘⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⡇⠸⣧⡴⢡⣿⣿⣿⣿⣷⡶⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⢸⠁⣿⣿⣿⣿⣿⣿⣤⡆⠞⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣦⡁⣿⣿⣿⣿⣿⣿⢏⣴⠁⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⣠⣴⣿⣿⣿⣿⣿⣿⠸⣣⡇⠘⢋⣉⣀⡀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠠⠞⠋⣁⣤⢼⡍⣉⠛⣛⠛⣛⢁⡒⢯⣤⣙⠲⢮⣍⣛⠷⣄⡀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⣠⣴⠟⣋⣴⠟⣰⠏⢀⡟⠀⣷⠀⠙⢦⣈⠛⠿⣶⣤⣉⠛⠶⢦⣄⡀⠀
+# ⠀⠀⣠⡴⠛⢉⣤⠞⠋⣠⡾⠃⠀⣼⠃⠀⣿⠀⠀⠀⠙⢷⣤⡀⠙⠻⢿⣶⣦⡌⠉⠃
+# ⠐⠋⠁⠀⠐⠋⠀⣠⣾⠟⠁⠀⢰⡏⠀⠀⣿⡀⠀⠀⠀⠀⠻⢿⡆⠀⠀⠉⠛⠀⠀⠀
+# ⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠸⠁⠀⠀⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+
+
+
+
+def main():
+
+    hexacentroptimus = 0
+
+
+    ### wczytywanie
+    with open("dane.txt") as f:
+        for line in f:
+            parts = line.strip().split(",")
+    
+
+    ###
+    for a in parts:
+        start,end = a.split("-")
+        end = int(end)
+        start = int(start)
+
+        while start != end+1:    
+            
+            start = str(start)
+
+            
+            nanoomegalodoimus = False
+
+
+
+            for z in range(100):
+                if patrzyk(start,z+1):
+                    nanoomegalodoimus =True           
+            
+            if nanoomegalodoimus ==True:
+                hexacentroptimus = hexacentroptimus + int(start)
+                print(start)
+                
+            start = int(start)+1
+             
+            
+                 
+
+
+                
+            
+        
+
+            
+                     
+    print("Odp:",hexacentroptimus)              
+
+            
+def patrzyk(loddzik,szczepek):
+ 
+#liczba do analizy
+#ile skok
+
+ loddzik=str(loddzik)
+ megaoktohektoplazma = False
+ 
+ if len(loddzik) <= szczepek or len(loddzik) % szczepek != 0:
+     return False
+
+
+
+ for c in range(int(int(len(loddzik))/int(szczepek))-1):
+     
+     if loddzik[(len(loddzik)-szczepek-c*szczepek)-szczepek:(len(loddzik)-c*szczepek)-szczepek] != loddzik[len(loddzik)-szczepek-c*szczepek:len(loddzik)-c*szczepek]:
+
+        megaoktohektoplazma = True
+        
+                     
+ if megaoktohektoplazma:
+     return False
+ else:
+     
+     return True
+     
+
+    ###
+       
+main()
+
